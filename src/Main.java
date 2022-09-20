287ba26e01378ef2c6467c085311b8cfbe2bf95a
antonina <grigorevna.antoninka@mail.ru>
2022-09-20 23:07:50 +0700

    домашняя2

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        //задание 4
+        System.out.println("задание4");
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+        //задание5
+        System.out.println("задание5");
+        var frog=3.5;
+        System.out.println(frog);
+        frog=frog*10;
+        System.out.println(frog);
+        frog=frog/3.5;
+        System.out.println(frog);
+        frog=frog+4;
+        System.out.println(frog);
+
+
+    }
+}
